2e25da7fdf6a696d9b89942020e2f1f02c496b01
Leon Liang <leonlhliang@gmail.com>
2014-07-17 23:50:38 +0800

    More refined compilation

--- bin/compile.py
+++ bin/compile.py
@@ -3,7 +3,6 @@
 import codecs
 import json
 import sys
-import os
 
 
 def translate(cond):
@@ -11,13 +10,9 @@ def translate(cond):
     return "false"
 
 
-JSHEAD = "function (lane,alley,number,dash,floor) {'use strict';"
-JSHEAD += "var token=(lane)?lane:alley;"
-JSHEAD += "token=(token)?token:number;"
-
-JSBODY = "if (%s) return '%s';"
-
-JSTAIL = "return '00000';}"
+js_head = "function (n) {"
+js_body = "if (%s) return '%s';"
+js_tail = "return '00000';}"
 
 
 ifile = codecs.open("tmp/weighted.json", "r", encoding="utf-8")
@@ -28,13 +23,13 @@ ifile.close()
 for c in rules:
     for d in rules[c]:
         for s in rules[c][d]:
-            js = JSHEAD
+            js = js_head
             for rule in rules[c][d][s].pop("weighted"):
                 segs = rule.split(":")
                 condition = translate(segs[-1])
                 zipcode = segs[0]
-                js += JSBODY % (condition, zipcode)
-            js += JSTAIL
+                js += js_body % (condition, zipcode)
+            js += js_tail
             rules[c][d][s] = js
 
 
@@ -46,10 +41,15 @@ ofile.close()
 ifile = open("tmp/result.json", "r")
 ofile = open("lib/rule.js", "w")
 
-for line in ifile:
-    line = line.replace('": "function', '": function')
-    line = line.replace(';}"', ';}')
-    ofile.write(line)
+for index, line in enumerate(ifile):
+    if index == 0:
+        ofile.write("module.exports = {\n")
+    elif line == "}":
+        ofile.write("};\n")
+    else:
+        line = line.replace('": "function', '": function')
+        line = line.replace(';}"', ';}')
+        ofile.write(line)
 
 ofile.close()
 ifile.close()
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var rule = require("./lib/rule");
 
 
-var decompose = function (numbers) {
+var parse = function (numbers) {
     "use strict";
-    return numbers;
+    return {
+        number: 0,
+        alley: 0,
+        floor: 0,
+        index: 0,
+        lane: 0,
+        dash: 0
+    };
 };
 
 
-module.exports = function (city, district, street, rest) {
+module.exports = function (city, district, street, numbers) {
     "use strict";
-    return rule[city][district][street](decompose(rest));
+    return rule[city][district][street](parse(numbers));
 };
