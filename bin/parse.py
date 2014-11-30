d49b3ec8c93e7e84b9d2c2ed643820c97a486d95
Li-Heng Liang <leonlhliang@gmail.com>
2014-11-30 23:47:35 +0800

    Reorg

--- bin/parse.py
+++ bin/parse.py
@@ -4,17 +4,26 @@ import json
 import sys
 
 
-result = {}
-
 INDEX = {
     "AREA": 5 + 3 * 3 + 3 * 3,
     "CITY": 5 + 3 * 3,
     "CODE": 5
 }
 
-for line in open("ORIGIN.txt"):
-    code = line[:INDEX["CODE"]]
+result = {}
 
+for line in open("ORIGIN.txt"):
+    line = line.replace("１", "一")
+    line = line.replace("２", "二")
+    line = line.replace("３", "三")
+    line = line.replace("４", "四")
+    line = line.replace("５", "五")
+    line = line.replace("６", "六")
+    line = line.replace("７", "七")
+    line = line.replace("８", "八")
+    line = line.replace("９", "九")
+
+    code = line[:INDEX["CODE"]]
     city = line[INDEX["CODE"]: INDEX["CITY"]]
     if not city in result: result[city] = {}
 
@@ -29,16 +38,6 @@ for line in open("ORIGIN.txt"):
     if len(line.split(" ")) == 1: road = line[:-3]
 
     spec = line.replace(road, "").replace("　", "").replace(" ", "")
-
-    road = road.replace("１", "一")
-    road = road.replace("２", "二")
-    road = road.replace("３", "三")
-    road = road.replace("４", "四")
-    road = road.replace("５", "五")
-    road = road.replace("６", "六")
-    road = road.replace("７", "七")
-    road = road.replace("８", "八")
-    road = road.replace("９", "九")
 
     if not road in result[city][area]: result[city][area][road] = []
     result[city][area][road].append("%s:%s" % (code, spec))
