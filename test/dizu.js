55b9ec8615e698cd3575e78cdcf771521ecc328c
Li-Heng Liang <leonlhliang@gmail.com>
2014-12-06 08:18:59 +0800

    Refactor JS to be more promise-ish. Pre-commit tests should take less time

--- test/dizu.js
+++ test/dizu.js
@@ -1,24 +1,25 @@
 var promise = require("bluebird");
 var runexec = promise.promisify(require("child_process").exec);
-var fsrdir  = promise.promisify(require("fs").readdir);
-var fsopen  = promise.promisify(require("fs").open);
-var expect  = require("chai").expect;
+var chai    = require("chai");
 var path    = require("path");
+var fs      = promise.promisifyAll(require("fs"));
+
+
+chai.use(require("chai-as-promised"));
 
 
 module.exports = function () {
 
-    this.Given(/^official documents are in place:$/, function (table, next) {
-        expect(table.raw().length).to.equal(2);
-        expect(table.raw().filter(function (row) {
-            var filepath = path.join(process.cwd(), row[0]);
-            return fsopen(filepath, "r").then(function () {
-                return true;
-            }).catch(function () {
-                return false;
+    this.Given(/^required documents are in place:$/, function (table, next) {
+        return promise.all(table.raw().map(function (row) {
+            return row.map(function (col) {
+                return fs.openAsync(path.join(process.cwd(), col), "r");
             });
-        }).length).to.equal(table.raw().length);
-        return next();
+        })).then(function () {
+            return next();
+        }).catch(function (err) {
+            return next.fail(err);
+        });
     });
 
     this.When(/^execute the command "([^"]*)"$/, function (cmd, next) {
@@ -31,30 +32,37 @@ module.exports = function () {
 
     this.Then(/^have a valid JSON at "([^"]*)"$/, function (file, next) {
         var filepath = path.join(process.cwd(), file);
-        expect(require(filepath)).to.be.an("object");
-        return next();
+        return fs.readFileAsync(filepath).then(function (content) {
+            JSON.parse(content);
+            return next();
+        }).catch(function (err) {
+            return next.fail(err);
+        });
     });
 
     this.Then(/^file "([^"]*)" holds sample:$/, function (file, table, next) {
-        var filepath = path.join(process.cwd(), file), name = null;
+        var filepath = path.join(process.cwd(), file);
 
-        expect(require(filepath)).to.be.an("object");
-        name = require(filepath);
-
-        table.raw().forEach(function (row) {
-            expect(name).to.have.property(row[0]);
-            expect(name[row[0]]["name"][0]).to.equal(row[1]);
-            expect(name[row[0]]["name"][1]).to.equal(row[2]);
+        return fs.readFileAsync(filepath).then(function (content) {
+            var name = JSON.parse(content);
+            table.raw().forEach(function (row) {
+                chai.expect(name).to.have.property(row[0]).
+                and.to.have.property("name").
+                and.to.be.an("array").
+                and.to.deep.equal([row[1], row[2]]);
+            });
+            return next();
+        }).catch(function (err) {
+            return next.fail(err);
         });
-
-        return next();
     });
 
     this.Then(/^folder "([^"]*)" holds folders:$/, function (dir, table, next) {
-        return fsrdir(path.join(process.cwd(), dir)).then(function (dirs) {
+        var target = path.join(process.cwd(), dir);
+        return fs.readdirAsync(target).then(function (dirs) {
             table.raw().forEach(function (row) {
                 row.forEach(function (col) {
-                    expect(dirs).to.include(col);
+                    chai.expect(dirs).to.include(col);
                 });
             });
             return next();
@@ -64,17 +72,16 @@ module.exports = function () {
     });
 
     this.Then(/^each area in "([^"]*)" be one "([^"]*)" file$/, function (file, ext, next) {
-        var filepath = path.join(process.cwd(), file), areas = [], map = {};
+        var filepath = path.join(process.cwd(), file);
 
-        expect(require(filepath)).to.be.an("object");
-        map = require(filepath);
-
-        for (var city in map) {for (var area in map[city].area) {
-            var target = path.join("lib", city, [area, ext].join("."));
-            areas.push(fsopen(target, "r"));
-        }}
-
-        return promise.all(areas).then(function () {
+        return fs.readFileAsync(filepath).then(function (content) {
+            var map = JSON.parse(content), areas = [];
+            for (var city in map) {for (var area in map[city].area) {
+                var target = path.join("lib", city, [area, ext].join("."));
+                areas.push(fs.openAsync(target, "r"));
+            }}
+            return promise.all(areas);
+        }).then(function () {
             return next();
         }).catch(function (err) {
             return next.fail(err);
